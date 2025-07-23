479b5b0a02dee52a57a0b785cf9737c489f50e0a
AgungDH <agungdh@live.com>
2025-07-23 08:37:42 +0700

    feat: integrate header and footer partials into guest layout
    - Replaced inline HTML in `guest.html` with `<app-header>` and `<app-footer>` components
    - Imported `Header` and `Footer` components in `guest.ts`
    - Implemented full header and footer UI in respective HTML files
    - Added dynamic year display in footer component

--- src/app/layouts/guest/guest.ts
+++ src/app/layouts/guest/guest.ts
@@ -1,11 +1,15 @@
 import {Component, signal} from '@angular/core';
+import {Footer} from './partials/footer/footer';
+import {Header} from './partials/header/header';
 
 @Component({
   selector: 'app-guest',
-  imports: [],
+  imports: [
+    Footer,
+    Header
+  ],
   templateUrl: './guest.html',
   styleUrl: './guest.css'
 })
 export class Guest {
-  protected readonly year = signal(new Date().getFullYear());
 }
