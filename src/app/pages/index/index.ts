fca2e578911285aa739316c0eba5e9a0efc40f0b
AgungDH <agungdh@live.com>
2025-07-23 11:55:15 +0700

    feat: integrate HttpClient(fetch) and implement text data fetch in Index page
    - Register `provideHttpClient(withFetch())` in `app.config.ts`
    - Update `Index` component to:
      - Inject `HttpClient`
      - Implement `OnInit` lifecycle
      - Fetch plain-text response with `{ responseType: 'text' }`

--- src/app/pages/index/index.ts
+++ src/app/pages/index/index.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Guest} from '../../layouts/guest/guest';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-index',
@@ -9,6 +10,16 @@ import {Guest} from '../../layouts/guest/guest';
   templateUrl: './index.html',
   styleUrl: './index.css'
 })
-export class Index {
+export class Index implements OnInit {
+  private readonly httpClient: HttpClient = inject(HttpClient)
 
+  ngOnInit(): void {
+    this.getData()
+  }
+
+  getData() {
+    this.httpClient.get('https://api.agungdh.my.id', {responseType: "text"}).subscribe((response) => {
+      console.log(response);
+    })
+  }
 }
